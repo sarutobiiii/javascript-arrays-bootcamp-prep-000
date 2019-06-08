b3f7bbe12eb496ec5619290be5dffe824b08814e
oluwatobi balogun <tobibags19@gmail.com>
2019-06-08 21:32:57 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+    return[element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return[]
+}
+
+function accessElementInArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift(element)
+  return array
+}
