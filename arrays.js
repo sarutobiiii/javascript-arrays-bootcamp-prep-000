4a1b04d99281cb65752f8b8fcb8f8920aef3a677
oluwatobi balogun <tobibags19@gmail.com>
2019-06-08 21:48:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element)
   array
 }
